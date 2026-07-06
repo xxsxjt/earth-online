79f080a76c0214462c1f8e4f2f38a09ac1c5e8c4
Zaxl Da <du_jia_xi@outlook.com>
2026-07-06 13:23:49 +0800

    Use vanilla-style machine screen texture

--- neoforge-26.2/src/main/java/com/xxsx/earthonline/client/ProcessingMachineScreen.java
+++ neoforge-26.2/src/main/java/com/xxsx/earthonline/client/ProcessingMachineScreen.java
@@ -9,8 +9,10 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.api.distmarker.Dist;
@@ -20,15 +22,11 @@ import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 public class ProcessingMachineScreen extends AbstractContainerScreen<ProcessingMachineMenu> {
-    private static final int VANILLA_PANEL = 0xFFC6C6C6;
-    private static final int VANILLA_LIGHT = 0xFFFFFFFF;
-    private static final int VANILLA_DARK = 0xFF555555;
-    private static final int VANILLA_SLOT = 0xFF8B8B8B;
+    private static final Identifier BG_LOCATION = Identifier.fromNamespaceAndPath("earth_online", "textures/gui/container/processing_machine.png");
+    private static final Identifier PROGRESS_SPRITE = Identifier.withDefaultNamespace("container/furnace/burn_progress");
     private static final int VANILLA_TEXT = 0xFF404040;
     private static final int MUTED = 0xFF606060;
     private static final int WARNING = 0xFFAA3322;
-    private static final int PROGRESS_BG = 0xFF8B8B8B;
-    private static final int PROGRESS = 0xFFFFD36A;
 
     private Button redstoneButton;
 
@@ -43,6 +41,7 @@ public class ProcessingMachineScreen extends AbstractContainerScreen<ProcessingM
     @Override
     protected void init() {
         super.init();
+        this.titleLabelX = Math.max(8, (this.imageWidth - this.font.width(trimmedTitle())) / 2);
         redstoneButton = addRenderableWidget(Button.builder(redstoneButtonLabel(), b -> cycleRedstoneMode())
                 .bounds(this.leftPos + 8, this.topPos + 55, 64, 17)
                 .build());
@@ -65,9 +64,7 @@ public class ProcessingMachineScreen extends AbstractContainerScreen<ProcessingM
     @Override
     public void extractBackground(GuiGraphicsExtractor g, int mouseX, int mouseY, float delta) {
         super.extractBackground(g, mouseX, mouseY, delta);
-        drawVanillaPanel(g);
-        drawSlot(g, 38, 35);
-        drawOutputSlots(g);
+        g.blit(RenderPipelines.GUI_TEXTURED, BG_LOCATION, this.leftPos, this.topPos, 0.0F, 0.0F, this.imageWidth, this.imageHeight, 256, 256);
         drawProgress(g);
     }
 
@@ -80,59 +77,15 @@ public class ProcessingMachineScreen extends AbstractContainerScreen<ProcessingM
     @Override
     protected void extractLabels(GuiGraphicsExtractor g, int mouseX, int mouseY) {
         g.text(this.font, trimmedTitle(), this.titleLabelX, this.titleLabelY, VANILLA_TEXT, false);
-        g.text(this.font, Component.translatable("screen.earth_online.machine.input"), 31, 24, MUTED, false);
-        g.text(this.font, Component.translatable("screen.earth_online.machine.outputs"), 112, 11, MUTED, false);
-        g.text(this.font, Component.translatable("screen.earth_online.machine.redstone"), 8, 45, MUTED, false);
         g.text(this.font, this.playerInventoryTitle, this.inventoryLabelX, this.inventoryLabelY, VANILLA_TEXT, false);
         g.text(this.font, statusLine(), 78, 59, statusColor(), false);
     }
 
-    private void drawOutputSlots(GuiGraphicsExtractor g) {
-        int[][] outputSlots = {
-                {102, 20}, {120, 20}, {138, 20}, {156, 20},
-                {111, 42}, {129, 42}, {147, 42}
-        };
-        for (int[] slot : outputSlots) {
-            drawSlot(g, slot[0], slot[1]);
-        }
-    }
-
-    private void drawVanillaPanel(GuiGraphicsExtractor g) {
-        int x = this.leftPos;
-        int y = this.topPos;
-        g.fill(x, y, x + this.imageWidth, y + this.imageHeight, VANILLA_PANEL);
-        g.fill(x, y, x + this.imageWidth, y + 1, VANILLA_LIGHT);
-        g.fill(x, y, x + 1, y + this.imageHeight, VANILLA_LIGHT);
-        g.fill(x + this.imageWidth - 1, y, x + this.imageWidth, y + this.imageHeight, VANILLA_DARK);
-        g.fill(x, y + this.imageHeight - 1, x + this.imageWidth, y + this.imageHeight, VANILLA_DARK);
-        g.fill(x + 7, y + 82, x + 169, y + 83, VANILLA_DARK);
-    }
-
-    private void drawSlot(GuiGraphicsExtractor g, int x, int y) {
-        int sx = this.leftPos + x - 1;
-        int sy = this.topPos + y - 1;
-        g.fill(sx, sy, sx + 18, sy + 18, VANILLA_DARK);
-        g.fill(sx + 1, sy + 1, sx + 17, sy + 17, VANILLA_SLOT);
-        g.fill(sx + 1, sy + 1, sx + 16, sy + 2, 0xFF373737);
-        g.fill(sx + 1, sy + 1, sx + 2, sy + 16, 0xFF373737);
-        g.fill(sx + 2, sy + 2, sx + 17, sy + 17, 0xFFE0E0E0);
-        g.fill(sx + 2, sy + 2, sx + 16, sy + 16, VANILLA_SLOT);
-    }
-
     private void drawProgress(GuiGraphicsExtractor g) {
-        int x = this.leftPos + 64;
-        int y = this.topPos + 38;
-        drawArrow(g, x, y, PROGRESS_BG);
         int progress = Math.min(24, 24 * this.menu.progress() / this.menu.maxProgress());
         if (progress > 0) {
-            g.fill(x, y + 5, x + progress, y + 12, this.menu.active() ? PROGRESS : 0xFFFFB347);
+            g.blitSprite(RenderPipelines.GUI_TEXTURED, PROGRESS_SPRITE, 24, 16, 0, 0, this.leftPos + 64, this.topPos + 38, progress, 16);
         }
-    }
-
-    private void drawArrow(GuiGraphicsExtractor g, int x, int y, int color) {
-        g.fill(x, y + 5, x + 18, y + 12, color);
-        g.fill(x + 18, y + 2, x + 21, y + 15, color);
-        g.fill(x + 21, y + 5, x + 24, y + 12, color);
     }
 
     private String statusLine() {
